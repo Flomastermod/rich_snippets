e08357cd025353b589bcb941fede4d7d2a7deb6c
sampiecz <samuel.piecz@gmail.com>
2018-07-11 17:53:33 -0500

    Generated script tag and pack data into it.

--- rich_snippet.js
+++ rich_snippet.js
@@ -1,17 +1,30 @@
+// Generate the variables needed for an article Rich Snippet
 var article_h1 = document.getElementsByTagName("h1")[0];
 var article_url = document.URL;
 var images = document.getElementsByClassName("rich-image");
 var image_list = [];
 
+// Pack all images into images array
 for(var i = 0; i < images.length; i++)
 {
     image_list[i] = images[i].src;
 }
 
+// Pack the rich snippet variable with our article's data
 var rich_snippet = {
   "@context": article_url,
   "@type": "Article",
   "headline": article_h1.textContent,
-  "image": image_list, 
+  "image": image_list 
 }
 
+// Generate a script object to append to the closing body tag
+rich_script = document.createElement("script");
+
+// Give the script a class so I can select it easily
+rich_script.classList.add("rich-script");
+
+// Pack the rich_snippet into the rich_script
+rich_script.innerHTML = rich_snippet;
+
+// Append the rich_script to the closing body tag
