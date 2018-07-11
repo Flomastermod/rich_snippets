ceb867d5472c086ee5340e3245627fa0ab16db84
sampiecz <samuel.piecz@gmail.com>
2018-07-11 18:11:48 -0500

    Append the stringified json object to the closing body tag.

--- rich_snippet.js
+++ rich_snippet.js
@@ -8,15 +8,18 @@ var image_list = [];
 for(var i = 0; i < images.length; i++)
 {
     image_list[i] = images[i].src;
-}
+};
 
 // Pack the rich snippet variable with our article's data
 var rich_snippet = {
   "@context": article_url,
   "@type": "Article",
   "headline": article_h1.textContent,
-  "image": image_list 
-}
+  "image": image_list,
+};
+
+// Convert object to string
+rich_snippet_raw = JSON.stringify(rich_snippet)
 
 // Generate a script object to append to the closing body tag
 rich_script = document.createElement("script");
@@ -25,6 +28,7 @@ rich_script = document.createElement("script");
 rich_script.classList.add("rich-script");
 
 // Pack the rich_snippet into the rich_script
-rich_script.innerHTML = rich_snippet;
+rich_script.innerHTML = rich_snippet_raw;
 
 // Append the rich_script to the closing body tag
+document.body.appendChild(rich_script);
