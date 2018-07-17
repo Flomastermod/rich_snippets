4f550649ebf90c880d8576e770cabc227614e5c5
Samuel Piecz <samuel.piecz@gmail.com>
2018-07-17 12:05:39 -0500

    Added datemodified and the mainEntityOfPage. Date field is hardcoded, will add if statement to check for rich-date eventually.

--- rich_snippet_article.js
+++ rich_snippet_article.js
@@ -1,25 +1,26 @@
-<script type='text/javascript' defer>
-	// Generate the variables needed for an article Rich Snippet
-	var article_h1 = document.getElementsByTagName("h1")[0];
-	var images = document.getElementsByClassName("rich-image");
-	var image_list = [
-		"https://d3ves0vn0b2jei.cloudfront.net/wp-content/uploads/2017/12/Bento-business-debit-card.jpg",
-	];
+<script type='text/javascript' async>
+    // Generate the variables needed for an article Rich Snippet
+    var article_h1 = document.getElementsByTagName("h1")[0];
+    var images = document.getElementsByClassName("rich-image");
+    var image_list = [
+        "https://d3ves0vn0b2jei.cloudfront.net/wp-content/uploads/2017/12/Bento-business-debit-card.jpg",
+    ];
 
-	// Pack all images into images array
-	for(var i = 0; i < images.length; i++)
-	{
-		image_list.push(images[i].src);
-	};
+    // Pack all images into images array
+    for(var i = 0; i < images.length; i++)
+    {
+        image_list.push(images[i].src);
+    };
 
-	// Pack the rich snippet variable with our article's data
-	var rich_snippet = {
+    // Pack the rich snippet variable with our article's data
+    var rich_snippet = {
         "@context": "http://schema.org",
         "@type": "Article",
         "headline": article_h1.textContent,
         "image": image_list,
         "author": "Bento for Business",
-        "datePublished": "6/14/2018",
+        "datePublished": "6/17/2018",
+        "dateModified": "6/17/2018",
         "publisher": {
             "@type": "Organization",
             "name": "Bento for Business",
@@ -28,23 +29,27 @@
                 "url": "https://d3ves0vn0b2jei.cloudfront.net/wp-content/uploads/2018/02/bento-logo-retina-e1519672019840.png"
             }
         },
-	};
+        "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": document.URL,
+        },
+    };
 
-	// Convert object to string
-	rich_snippet_raw = JSON.stringify(rich_snippet)
+    // Convert object to string
+    rich_snippet_raw = JSON.stringify(rich_snippet)
 
-	// Generate a script object to append to the closing body tag
-	rich_script = document.createElement("script");
+    // Generate a script object to append to the closing body tag
+    rich_script = document.createElement("script");
 
-	// Give the script a class so I can select it easily
-	rich_script.classList.add("rich-script");
+    // Give the script a class so I can select it easily
+    rich_script.classList.add("rich-script");
 
-	// Give the script the appropriate type for a rich snippet
-	rich_script.type = "application/ld+json"
+    // Give the script the appropriate type for a rich snippet
+    rich_script.type = "application/ld+json"
 
-	// Pack the rich_snippet into the rich_script
-	rich_script.innerHTML = rich_snippet_raw;
+    // Pack the rich_snippet into the rich_script
+    rich_script.innerHTML = rich_snippet_raw;
 
-	// Append the rich_script to the closing body tag
-	document.body.appendChild(rich_script);
+    // Append the rich_script to the closing body tag
+    document.body.appendChild(rich_script);
 </script>
